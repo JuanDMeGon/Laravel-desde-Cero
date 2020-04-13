0c5158fc7ed5f9611610244d2d799c41535e033d
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-13 17:09:26 -0500

    Creando múltiples tipos de rutas y acciones en Laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('main');
+
+Route::get('products', function () {
+    return 'This is the list of products';
+})->name('products.index');
+
+Route::get('products/create', function () {
+    return 'This is the form to create a product';
+})->name('products.create');
+
+Route::post('products', function () {
+    //
+})->name('products.store');
+
+Route::get('products/{product}', function ($product) {
+    return "Showing product with id {$product}";
+})->name('products.show');
+
+Route::get('products/{product}/edit', function ($product) {
+    return "Showing the form to edit the product with id {$product}";
+})->name('products.edit');
+
+Route::match(['put', 'patch'], 'products/{product}', function ($product) {
+    //
+})->name('products.update');
+
+Route::delete('products/{product}', function ($product) {
+    //
+})->name('products.destroy');
