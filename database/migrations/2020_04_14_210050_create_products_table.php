7eefea70e6b4db579dc7ce8ea7832a7744333640
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-14 16:13:31 -0500

    Creación de tablas con migraciones de Laravel

---
+++ database/migrations/2020_04_14_210050_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description', 1000);
+            $table->float('price')->unsigned();
+            $table->integer('stock')->unsigned();
+            $table->string('status')->default('unavailable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
