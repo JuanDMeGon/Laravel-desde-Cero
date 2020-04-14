efd52633277198c726128e3e94c059604e2c54d9
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-14 16:36:42 -0500

    Introduciendo Eloquent y los modelos en Laravel

---
+++ app/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'stock',
+        'status',
+    ];
+}
