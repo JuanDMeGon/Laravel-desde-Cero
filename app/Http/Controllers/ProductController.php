870390c96627419ef6679a216cace17420e42257
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-15 17:31:10 -0500

    Validando datos antes de realizar modificaciones con Eloquent

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -20,6 +20,16 @@ class ProductController extends Controller
 
     public function store()
     {
+        $rules = [
+            'title' => ['required', 'max:255'],
+            'description' => ['required', 'max:1000'],
+            'price' => ['required', 'min:1'],
+            'stock' => ['required', 'min:0'],
+            'status' => ['required', 'in:available,unavailable'],
+        ];
+
+        request()->validate($rules);
+
         if (request()->status == 'available' && request()->stock == 0) {
             // session()->put('error', 'If available must have stock');
             session()->flash('error', 'If available must have stock');
@@ -50,6 +60,16 @@ class ProductController extends Controller
 
     public function update($product)
     {
+        $rules = [
+            'title' => ['required', 'max:255'],
+            'description' => ['required', 'max:1000'],
+            'price' => ['required', 'min:1'],
+            'stock' => ['required', 'min:0'],
+            'status' => ['required', 'in:available,unavailable'],
+        ];
+
+        request()->validate($rules);
+
         $product = Product::findOrFail($product);
 
         $product->update(request()->all());
--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -8,23 +8,23 @@
         @method('PUT')
         <div class="form-row">
             <label>Title</label>
-            <input class="form-control" type="text" name="title" value="{{ $product->title }}" required>
+            <input class="form-control" type="text" name="title" value="{{ $product->title }}" >
         </div>
         <div class="form-row">
             <label>Description</label>
-            <input class="form-control" type="text" name="description" value="{{ $product->description }}" required>
+            <input class="form-control" type="text" name="description" value="{{ $product->description }}" >
         </div>
         <div class="form-row">
             <label>Price</label>
-            <input class="form-control" type="number" min="1.00" step="0.01" name="price" value="{{ $product->price }}" required>
+            <input class="form-control" type="number" min="1.00" step="0.01" name="price" value="{{ $product->price }}" >
         </div>
         <div class="form-row">
             <label>Stock</label>
-            <input class="form-control" type="number" min="0" name="stock" value="{{ $product->stock }}" required>
+            <input class="form-control" type="number" min="0" name="stock" value="{{ $product->stock }}" >
         </div>
         <div class="form-row">
             <label>Status</label>
-            <select class="custom-select" name="status" required>
+            <select class="custom-select" name="status" >
                 <option {{ $product->status == 'available' ? 'selected' : '' }} value="available">Available</option>
                 <option {{ $product->status == 'unavailable' ? 'selected' : '' }} value="unavailable">Unavailable</option>
             </select>
