4782e428b57706921ebfc82d55d63658e0241a83
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-15 16:04:46 -0500

    Repasando algunos conceptos para mejorar la usabilidad del proyecto Laravel

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,6 +3,8 @@
 @section('content')
     <h1>List of Products</h1>
 
+    <a class="btn btn-success" href="{{ route('products.create') }}">Create</a>
+
     @empty ($products)
         <div class="alert alert-warning">
             The list of products is empty
@@ -18,6 +20,7 @@
                         <th>Price</th>
                         <th>Stock</th>
                         <th>Status</th>
+                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -29,6 +32,11 @@
                         <td>{{ $product->price }}</td>
                         <td>{{ $product->stock }}</td>
                         <td>{{ $product->status }}</td>
+                        <td>
+                            <a class="btn btn-link" href="{{ route('products.show', ['product' => $product->id]) }}">Show</a>
+
+                            <a class="btn btn-link" href="{{ route('products.edit', ['product' => $product->id]) }}">Edit</a>
+                        </td>
                     </tr>
                     @endforeach
                 </tbody>
