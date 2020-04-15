ca9b080255d2ec784302529281387473a8d25889
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-15 14:15:16 -0500

    Conoce las estructuras de control esenciales de Blade

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,9 +8,9 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::all();
-
-        return view('products.index');
+        return view('products.index')->with([
+            'products' => Product::all(),
+        ]);
     }
 
     public function create()
