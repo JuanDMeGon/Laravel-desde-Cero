7074b067f90baeddc5c5e05e74770ac822438fc8
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-15 17:40:55 -0500

    Permitiendo mostrar errores en las vistas con Blade

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,12 +5,22 @@
     <title>Learn Laravel</title>
 </head>
 <body>
-    @dump($errors)
     @if (session()->has('error'))
         <div class="alert alert-danger">
             {{ session()->get('error') }}
         </div>
     @endif
+
+    @if (isset($errors) && $errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     @yield('content')
 </body>
 </html>
