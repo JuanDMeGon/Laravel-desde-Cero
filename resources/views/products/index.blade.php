ae2c614fe21ca2a43663c7715c4e177b01c817b0
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-15 14:15:16 -0500

    Conoce las estructuras de control esenciales de Blade

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,28 +7,37 @@
 <body>
     <h1>List of Products</h1>
 
-    <div class="table-responsive">
-        <table class="table table-striped">
-            <thead class="thead-light">
-                <tr>
-                    <th>Id</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Soap</td>
-                    <td>Best soap ever</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Shampoo</td>
-                    <td>Best shampoo ever</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+    @empty ($products)
+        <div class="alert alert-warning">
+            The list of products is empty
+        </div>
+    @else
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead class="thead-light">
+                    <tr>
+                        <th>Id</th>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                        <th>Stock</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                    <tr>
+                        <td>{{ $product->id }}</td>
+                        <td>{{ $product->title }}</td>
+                        <td>{{ $product->description }}</td>
+                        <td>{{ $product->price }}</td>
+                        <td>{{ $product->stock }}</td>
+                        <td>{{ $product->status }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    @endempty
 </body>
 </html>
