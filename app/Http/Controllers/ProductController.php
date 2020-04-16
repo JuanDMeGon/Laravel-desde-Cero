a35857e7e7028d0eab09dbfb6270b39ebb9fa0cf
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-16 16:51:07 -0500

    Los middleware y cómo restringir el acceso en Laravel

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,11 @@ use App\Product;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('products.index')->with([
