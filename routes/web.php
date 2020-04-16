a44bf401fbd3906d279a88950fa8d3819b758637
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-16 17:16:47 -0500

    Mejorando las rutas usando rutas de recurso en Laravel

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'MainController@index')->name('main');
 
-Route::get('products', 'ProductController@index')->name('products.index');
-
-Route::get('products/create', 'ProductController@create')->name('products.create');
-
-Route::post('products', 'ProductController@store')->name('products.store');
-
-Route::get('products/{product}', 'ProductController@show')->name('products.show');
-// Route::get('products/{product:title}', 'ProductController@show')->name('products.show');
-
-Route::get('products/{product}/edit', 'ProductController@edit')->name('products.edit');
-
-Route::match(['put', 'patch'], 'products/{product}', 'ProductController@update')->name('products.update');
-
-Route::delete('products/{product}', 'ProductController@destroy')->name('products.destroy');
+Route::resource('products', 'ProductController');
 
 Auth::routes();
 
