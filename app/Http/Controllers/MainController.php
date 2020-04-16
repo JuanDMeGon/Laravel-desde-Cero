f583bb61b3b96f54121905fbdcca5be5aaf0b211
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-16 18:16:54 -0500

    Aprovechando los layouts y componentes con Blade de Laravel

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
+
 class MainController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        return view('welcome')->with([
+            'products' => Product::all(),
+        ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,17 @@
 
 @section('content')
     <h1>Welcome</h1>
-    <p>Let's start.</p>
+    @empty ($products)
+        <div class="alert alert-danger">
+            No products yet!
+        </div>
+    @else
+        <div class="row">
+            @foreach ($products as $product)
+                <div class="col-3">
+                    @include('components.product-card')
+                </div>
+            @endforeach
+        </div>
+    @endempty
 @endsection
