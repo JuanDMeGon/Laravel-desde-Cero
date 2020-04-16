755d3e3ba93376ef1b4935bbb5e2db1e4fc1aa19
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-16 18:02:04 -0500

    Usando Artisan de Laravel para crear múltiples componentes a la vez

---
+++ database/factories/CartFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Cart;
+use Faker\Generator as Faker;
+
+$factory->define(Cart::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
