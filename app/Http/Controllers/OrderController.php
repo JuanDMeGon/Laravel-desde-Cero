af8259875d58081561aa0375a42362068051233b
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-18 18:43:08 -0500

    Identificando el usuario autenticado con Laravel para crear una orden

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -12,6 +12,8 @@ class OrderController extends Controller
     public function __construct(CartService $cartService)
     {
         $this->cartService = $cartService;
+
+        $this->middleware('auth');
     }
 
     /**
@@ -42,6 +44,22 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = $request->user();
+
+        $order = $user->orders()->create([
+            'status' => 'pending',
+        ]);
+
+        $cart = $this->cartService->getFromCookie();
+
+        $cartProductsWithQuantity = $cart
+            ->products
+            ->mapWithKeys(function ($product) {
+                $element[$product->id] = ['quantity' => $product->pivot->quantity];
+
+                return $element;
+            });
+
+        $order->products()->attach($cartProductsWithQuantity->toArray());
     }
 }
