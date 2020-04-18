4682dac26a39701d7becfbff825ffb0328187eea
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-18 14:20:27 -0500

    Usando e insertando relaciones polimórficas en el seeder de Laravel

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Cart;
+use App\Image;
 use App\Order;
 use App\Payment;
 use App\Product;
@@ -15,7 +17,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $users = factory(User::class, 20)->create();
+        $users = factory(User::class, 20)
+            ->create()
+            ->each(function ($user) {
+                $image = factory(Image::class)
+                    ->states('user')
+                    ->make();
+
+                $user->image()->save($image);
+            });
 
         $orders = factory(Order::class, 10)
             ->make()
@@ -29,6 +39,25 @@ class DatabaseSeeder extends Seeder
                 $order->payment()->save($payment);
             });
 
-        $products = factory(Product::class, 50)->create();
+        $carts = factory(Cart::class, 20)->create();
+
+        $products = factory(Product::class, 50)
+            ->create()
+            ->each(function ($product) use ($orders, $carts) {
+                $order = $orders->random();
+
+                $order->products()->attach([
+                    $product->id => ['quantity' => mt_rand(1, 3)],
+                ]);
+
+                $cart = $carts->random();
+
+                $cart->products()->attach([
+                    $product->id => ['quantity' => mt_rand(1, 5)],
+                ]);
+
+                $images = factory(Image::class, mt_rand(2, 4))->make();
+                $product->images()->saveMany($images);
+            });
     }
 }
