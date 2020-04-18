c480b4a38fdb1d103936b2cc992741d8de0c2af5
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-18 18:43:08 -0500

    Identificando el usuario autenticado con Laravel para crear una orden

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -5,6 +5,17 @@
 
     <h4 class="text-center"><strong>Grand Total: </strong> {{ $cart->total }}</h4>
 
+    <div class="text-center mb-3">
+        <form
+            class="d-inline"
+            method="POST"
+            action="{{ route('orders.store') }}"
+        >
+            @csrf
+            <button type="submit" class="btn btn-success">Confirm Order</button>
+        </form>
+    </div>
+
     <div class="table-responsive">
         <table class="table table-striped">
             <thead class="thead-light">
