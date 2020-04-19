63c3b6e527655ffb67995568439bdc376293023d
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-04-18 19:07:43 -0500

    Usa un controlador de recursos anidados de Laravel para cobrar una orden

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::resource('carts', 'CartController')->only(['index']);
 
 Route::resource('orders', 'OrderController')->only(['create', 'store']);
 
+Route::resource('orders.payments', 'OrderPaymentController')->only(['create', 'store']);
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
