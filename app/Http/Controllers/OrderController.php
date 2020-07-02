80246bc6be80e40b1e23763dadf911dc90208e88
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-07-01 19:54:16 -0500

    Manejando el stock de los productos comprados

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Services\CartService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 
 class OrderController extends Controller
 {
@@ -57,7 +58,16 @@ class OrderController extends Controller
             $cartProductsWithQuantity = $cart
                 ->products
                 ->mapWithKeys(function ($product) {
-                    $element[$product->id] = ['quantity' => $product->pivot->quantity];
+                    $quantity = $product->pivot->quantity;
+
+                    if ($product->stock < $quantity) {
+                        throw ValidationException::withMessages([
+                            'product' => "There is not enough stock for the quantity you required of {$product->title}",
+                        ]);
+                    }
+
+                    $product->decrement('stock', $quantity);
+                    $element[$product->id] = ['quantity' => $quantity];
 
                     return $element;
                 });
